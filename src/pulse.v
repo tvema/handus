1a3a0e049aa7f9dc551cc644d2e2a58983fe5c2b
Workspace User <workspace@example.com>
2026-06-08 19:38:26 +0300

    add pulse param

---
+++ src/pulse.v
@@ -0,0 +1,14 @@
+// New file
+module pulse(
+    input               rst_n,
+    input               clk,
+
+    input   [15:0]      i_charge,
+    input   [15:0]      i_transmit,
+    input   [15:0]      i_pulse,
+
+    output              o_turn_on,
+    output              o_strike
+);
+
+endmodule
